6c43b1632db2c494970223fd9f35a7ec26f02adb
Marie-Cathrine Sørensen <42997812+mari342e@users.noreply.github.com>
2019-05-13 14:41:15 +0200

    Deleting Umbraco API project

--- fysapp-umbraco-api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace fysapp_umbraco_api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- fysapp-umbraco-api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fysapp_umbraco_api.WebApiApplication" Language="C#" %>
